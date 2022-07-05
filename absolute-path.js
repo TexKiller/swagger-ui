0f944ce1bc2f3ef4e36ec371ccb8f8ccfa082c24
Faister Cabrera Carvalho <Tex.Killer@hotmail.com>
2022-07-05 00:20:00 -0300

    Add support for yarn workspaces

--- absolute-path.js
+++ absolute-path.js
@@ -13,7 +13,12 @@ const getAbsoluteFSPath = function () {
     const i1 = stack[0].indexOf("(");
     const i1b = stack[0].indexOf("/", (i1 > -1 ? i1 + 1 : "at ".length) + "webpack://".length) + 1;
     const i2 = stack[0].indexOf(":", i1 + "webpack://".length);
-    return require("path").resolve(__dirname + "/../" + stack[0].substring(i1b, stack[0].lastIndexOf("/", i2)).replace(/\\/g, "/"))
+    let fywr;
+    try {
+      fywr = require("find-yarn-workspace-root");
+    } catch (e) {}
+    const resolve = require("path").resolve;
+    return resolve((fywr && fywr(resolve(__dirname + "/..")) || __dirname + "/..") + "/" + stack[0].substring(i1b, stack[0].lastIndexOf("/", i2)).replace(/\\/g, "/"))
   }
   throw new Error('getAbsoluteFSPath can only be called within a Nodejs environment');
 }
